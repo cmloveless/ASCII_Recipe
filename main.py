cd0438b1d044a1e696fc916cd5d943c981e1fe19
Colin <colinmloveless@gmail.com>
2023-09-09 01:37:29 -0700

    Added ASCII to formula from file function

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 key = {'sp': ' ', 'bS': '\\', 'sQ': "\'"}
 
 
-def formula_to_ascii(formula):
+def formula_to_ascii(filename):
+
+    with open(filename, 'r') as file:
+        chunk = file.read()
 
     line = ''
-    chunk = formula.split(" ")
 
     for x in chunk:
         numeral = ''
@@ -27,3 +29,56 @@ def formula_to_ascii(formula):
             else:
                 line += (chars * int(numeral))
     return line
+
+
+def ascii_to_formula(filename):
+    # Read the content of the file
+    with open(filename, 'r') as file:
+        art = file.read()
+
+    # Abbreviations
+    abbreviations = {
+        ' ': 'sp',
+        '\\': 'bS',
+        '\'': 'sQ',
+    }
+
+    # Helper function to add current sequence to the result list
+    def add_to_result(count, char):
+        if char.isdigit():
+            result.append(str(count) + char)
+        elif char in abbreviations:
+            result.append(str(count) + abbreviations[char])
+        else:
+            result.append(str(count) + char)
+
+    # Initialization
+    result = []
+    current_char = None
+    count = 0
+
+    # Iterate through every character in art
+    for char in art:
+        # Handle newline
+        if char == '\n':
+            if current_char:
+                add_to_result(count, current_char)
+                count = 0
+                current_char = None
+            result.append('nl')
+        # Handle same character sequence
+        elif char == current_char:
+            count += 1
+        # Handle change of character
+        else:
+            if current_char:
+                add_to_result(count, current_char)
+            current_char = char
+            count = 1
+
+    # Handle the last sequence
+    if current_char:
+        add_to_result(count, current_char)
+
+    return ' '.join(result)
+
